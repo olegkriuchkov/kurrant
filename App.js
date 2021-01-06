e3f7a24a7d86be9e0f2c5ad6b609a8eb1bd9d47f
Oleg <okriuchkov@gmail.com>
2021-01-06 11:52:21 +0200

    feat: added tab nav

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from './src/pages/HomePage';
 import Log from './src/pages/Log';
 import Contacts from './src/pages/Contacts';
 
 const Stack = createStackNavigator();
 
+const Tab = createBottomTabNavigator();
+
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomePage} />
-        <Stack.Screen name="Contacts" component={Contacts} />
-        <Stack.Screen name="Log" component={Log} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomePage} />
+        <Tab.Screen name="Contacts" component={Contacts} />
+        <Tab.Screen name="Log" component={Log} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }

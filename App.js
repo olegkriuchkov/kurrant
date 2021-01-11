61af8b1ce4b2ed66021919339b367e9fc812445d
Oleg <okriuchkov@gmail.com>
2021-01-11 10:42:17 +0200

    feat: initial header

--- App.js
+++ App.js
@@ -5,16 +5,45 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from './src/pages/HomePage';
 import Log from './src/pages/Log';
 import Contacts from './src/pages/Contacts';
+import { Text, View } from 'react-native';
+import Header from './src/shared/Header';
 
 const Stack = createStackNavigator();
 
 const Tab = createBottomTabNavigator();
 
 function App() {
+  const rightButtons = [
+    {
+        id: 1,
+        color: 'rgba(255, 255, 255, 0.15)',
+        content: <Text>fg</Text>,
+        action: () => alert('First Right Button'),
+    },
+    {
+        id: 2,
+        color: 'rgba(255, 255, 255, 0.15)',
+        content: <Text>jjjj</Text>,
+        action: () => alert('Second Right Button'),
+    },
+  ];
   return (
     <NavigationContainer>
+      <Header/>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomePage} />
+        <Tab.Screen 
+          name="Home"
+          component={HomePage}
+          options={{
+            title: 'My home',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+        }}/>
         <Tab.Screen name="Contacts" component={Contacts} />
         <Tab.Screen name="Log" component={Log} />
       </Tab.Navigator>

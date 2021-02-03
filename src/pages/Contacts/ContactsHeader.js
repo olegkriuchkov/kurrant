ffc4e4bd3127cdceed12cfd752d7018246d73a13
svv1313 <seleznev19963@gmail.com>
2021-02-03 19:18:24 +0200

    Contact header fixed

--- src/pages/Contacts/ContactsHeader.js
+++ src/pages/Contacts/ContactsHeader.js
@@ -4,7 +4,6 @@ import {SearchBar} from 'react-native-elements';
 import {Actions} from 'react-native-router-flux';
 import Image from '../../components/Image';
 import COLOR from '../../constants/COLOR';
-import ContactsStyle from '../../style/page/ContactsStyle';
 
 const ContactsHeader = () => {
   const [searchValue, setSearchValue] = useState('');
@@ -13,7 +12,6 @@ const ContactsHeader = () => {
     <View style={styles.header}>
       <View style={styles.titleContainer}>
         <Text style={styles.headerText}>Contacts</Text>
-
         <TouchableOpacity>
           <Image
             onPress={() => Actions.AddFriendEntry()}
@@ -27,9 +25,9 @@ const ContactsHeader = () => {
         onChangeText={(value) => setSearchValue(value.toLowerCase())}
         value={searchValue}
         lightTheme={true}
-        inputStyle={ContactsStyle.searchField}
-        containerStyle={styles.searchContainer}
-        inputContainerStyle={styles.searchInput}
+        inputStyle={styles.searchInput}
+        containerStyle={styles.containerStyle}
+        inputContainerStyle={styles.inputContainerStyle}
         searchIcon={<Image path={require('../../assets/search.png')} />}
       />
     </View>
@@ -42,20 +40,33 @@ const styles = StyleSheet.create({
   header: {
     paddingTop: 50,
     paddingLeft: 18,
+    backgroundColor: COLOR.WHITE,
+    shadowColor: COLOR.BLACK,
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
   },
   headerText: {
     fontSize: 24,
     color: 'black',
   },
   containerStyle: {
-    backgroundColor: 'transparent',
-    padding: 10,
-    borderBottomWidth: 0,
+    backgroundColor: COLOR.WHITE,
     borderTopWidth: 0,
+    borderBottomWidth: 0,
+    padding: 0,
+    marginTop: 16,
+    marginBottom: 20,
+    marginRight: 18,
   },
   inputContainerStyle: {
-    backgroundColor: COLOR.WHITE,
+    backgroundColor: COLOR.LIGHT_GREY,
     borderRadius: 15,
+    paddingLeft: 18,
   },
   titleContainer: {
     width: '100%',
@@ -63,15 +74,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
   },
-  plusIcon: {width: 30, height: 30, marginRight: 10},
+  plusIcon: {width: 20, height: 20, marginRight: 18},
   searchContainer: {
     backgroundColor: 'transparent',
     padding: 10,
     borderColor: COLOR.WHITE,
   },
   searchInput: {
-    backgroundColor: 'white',
-    borderRadius: 15,
-    borderWidth: 2,
+    backgroundColor: COLOR.LIGHT_GREY,
   },
 });
